eecd403341a62f656c5414637281bcc676915f11
trinhtuanvuu@gmail.com <ttvtty2690>
2020-04-17 20:54:49 +0700

    initial commit

---
+++ app/src/main/java/com/tuanvuu/demotextview/MainActivity.java
@@ -0,0 +1,14 @@
+package com.tuanvuu.demotextview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.form_dangnhap);
+    }
+}
